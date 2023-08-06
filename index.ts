bf3b967ff0626595f27cff09f2eae2af1f60e6c2
Jonathon Rivers <jdr2045@gmail.com>
2023-08-06 17:47:09 -0400

    Setup JWT

--- index.ts
+++ index.ts
@@ -1,15 +1,16 @@
 import express from "express";
 import * as trpcExpress from "@trpc/server/adapters/express";
-import { appRouter } from "./trpc";
+import { appRouter, createContext } from "./trpc";
 
 const app = express();
 
 app.use(express.json());
 
 app.use(
-  "/trpc",
+  "/",
   trpcExpress.createExpressMiddleware({
     router: appRouter,
+    createContext,
   })
 );
 
--- trpc.ts
+++ trpc.ts
@@ -1,16 +1,36 @@
-import { initTRPC } from "@trpc/server";
+import { TRPCError, inferAsyncReturnType, initTRPC } from "@trpc/server";
 import { z } from "zod";
 import jwt from "jsonwebtoken";
 import "dotenv/config";
+import * as trpcExpress from "@trpc/server/adapters/express";
 
-const t = initTRPC.create();
+export const createContext = ({
+  req,
+  res,
+}: trpcExpress.CreateExpressContextOptions) => ({
+  token: req.headers["authorization"]
+    ? req.headers["authorization"].split(" ")[1]
+    : null,
+});
+type Context = inferAsyncReturnType<typeof createContext>;
+
+const t = initTRPC.context<Context>().create();
 
 export const appRouter = t.router({
   getToken: t.procedure
     .input(z.object({ userName: z.string().min(1) }))
     .mutation((opts) => {
-      return jwt.sign(opts.input, process.env.SECRET_TOKEN!);
+      return jwt.sign(opts.input, process.env.SECRET_TOKEN!, {
+        expiresIn: "1m",
+      });
     }),
+  getUser: t.procedure.query((opts) => {
+    if (opts.ctx.token) {
+      return jwt.verify(opts.ctx.token, process.env.SECRET_TOKEN!);
+    }
+
+    throw new TRPCError({ code: "UNAUTHORIZED" });
+  }),
 });
 
 export type AppRouter = typeof appRouter;
