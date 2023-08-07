9221ebd3c337197d6f2f8092250d3820f450e982
Jonathon Rivers <jdr2045@gmail.com>
2023-08-06 21:08:18 -0400

    Added CORS

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import * as trpcExpress from "@trpc/server/adapters/express";
 import { appRouter, createContext } from "./trpc";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use(
   "/",
